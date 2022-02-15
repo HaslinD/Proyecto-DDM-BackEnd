6ce14e4f10412832deddee3d83d05eb8460d50ce
LiHanlin <raptor43739@gmail.com>
2022-02-15 12:12:15 -0600

    Creacion Login y Registro
    Se crearon las bases para la pagina de login y registro

---
+++ Registro.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link href="css/style2.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>CarWash</title>
+
+  </head>
+
+  <body>
+
+    <header>
+      <div class="container">
+
+        <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="index.php"><img src="images/Logo_UNITEC.png" alt="Logo Terceto" class="img-fluid" width="10%"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+
+        </ul>
+
+        <ul class="navbar-nav ml-auto lista-redes">
+          <li class="nav-item">
+          <a class="nav-link" href="#"><i class="bi bi-facebook"></i></a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#"><i class="bi bi-twitter"></i></a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#"><i class="bi bi-instagram"></i></a>
+          </li>
+        </ul>
+        </div>
+      </nav>
+
+      </div>
+    </header>
+
+    <section id="Formulario">
+      <div class="container">
+
+        <!--<button class="open-button" onclick="openForm()">Open Form</button>-->
+
+        <div id="myForm">
+          <form method="post" action="Registro.php" class="form-container" enctype="multipart/form-data">
+            <h1>Nuevo Registro</h1>
+            <label for="nombre"><b>Nombre</b></label>
+            <input type="text" placeholder="Ingrese nombre" name="nombre" value="<?php if (isset($_POST['enviar'])){ echo htmlentities($_POST['nombre']); } ?>" required>
+
+            <label for="Usuario"><b>Numero de Telefono</b></label>
+            <input type="text" placeholder="Ingrese su numero Telefonico" name="numero" value="<?php if (isset($_POST['enviar'])){ echo htmlentities($_POST['numero']); } ?>" required>
+
+            <label for="Contraseña"><b>Fecha de Nacimiento</b></label>
+            <input type="date" placeholder="Ingrese Fecha de Nacimiento" name="fecha" value="<?php if (isset($_POST['enviar'])){ echo htmlentities($_POST['fechanac']); } ?>" required>
+
+            <label for="Email"><b>Correo electrónico</b></label>
+            <input type="text" placeholder="Ingrese su Correo" name="correo" value="<?php if (isset($_POST['enviar'])){ echo htmlentities($_POST['correo']); } ?>" required>
+
+            <label for="Contraseña"><b>Contraseña</b></label>
+            <input type="password" placeholder="Ingrese Contraseña" name="contra" value="<?php if (isset($_POST['enviar'])){ echo htmlentities($_POST['contra']); } ?>" required>
+
+            <button type="submit" name="enviar" value="submit" class="btn">REGISTRARSE</button>
+
+            <?php
+
+              require_once 'login.php';
+              $conn = new mysqli($hn, $un, $pw, $db);
+              if ($conn->connect_error) die("Error en Conexion");
+
+              //Verifica que los campos de nombre, apellido, cuenta, numero, correo y contra no esten vacios
+              if (isset($_POST['nombre']) && isset($_POST['numero']) && isset($_POST['fechanac']) && isset($_POST['correo']) && isset($_POST['contra'])) {
+
+                  //Recuperacion de las variables ingresadas en los campos
+                  $Nombre = get_post($conn, 'nombre');
+                  $NumTelefono = get_post($conn, 'numero');
+                  $FechadeNaci = get_post($conn, 'fechanac');
+                  $Correo = get_post($conn, 'correo');
+                  $Contraseña = get_post($conn, 'contra');
+
+                  //Recuperacion del correo y numero de cuenta en base al correo y numero de cuenta (Estan separados por fallas)
+                  $query = "SELECT num_telefono, correo FROM usuarios WHERE correo = '$Correo' ";
+                  $result = $conn->query($query);
+                  $query2 = "SELECT num_telefono, correo FROM usuarios WHERE num_cuenta = '$NumTelefono'";
+                  $result2 = $conn->query($query2);
+
+                  if (!$result) die("Fatal Error");
+                  if (!$result2) die("Fatal Error");
+
+                  $row = $result->fetch_array(MYSQLI_ASSOC);
+                  $row2 = $result2->fetch_array(MYSQLI_ASSOC);
+
+                  //Validacion que el numero de cuenta y el correo no existan en la base de datos
+                  if ($NumCuenta != $row2['num_telefono'] && $Correo != $row['correo']) {
+                    //Insercion a la tabla de usuarios con los campos ingresados
+                    $query = "INSERT INTO usuarios (nombre, num_telefono, fechanacimiento, correo, contrasenia)
+                              VALUES" . "('$Nombre','$NumTelefono','$Fechadenaci','$Correo','$Contraseña')";
+
+                    $result = $conn->query($query);
+                    if (!$result) echo "INSERT no se ha realizado<br><br>";
+
+                    //Al darle click al boton de registrars se redirige al usuario al login
+                    if (isset($_POST['enviar'])){
+                      ?>
+                        <script type="text/javascript">
+                          window.location = "index.php";
+                        </script>
+                      <?php
+                    }
+                  //Valida si el numero de cuenta y el correo ya existen en la base de datos
+                  } else if ($Correo == $row['correo'] && $NumTelefono == $row2['num_telefono']) {
+                    ?>
+                      <div class="alert">
+                        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                        <strong>Error!</strong> El numero de telefono y el correo que ingreso estan en uso.
+                      </div>
+                    <?php
+                  //Valida si el correo ya existe en la base de datos
+                  } else if($Correo == $row['correo']) {
+                    ?>
+                      <div class="alert">
+                        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                        <strong>Error!</strong> El correo electronico que ingreso esta en uso.
+                      </div>
+                    <?php
+                  //Valida si el numero de cuenta existe en la base de datos
+                  } else if($NumTelefono == $row2['num_telefono']) {
+                    ?>
+                      <div class="alert">
+                        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                        <strong>Error!</strong> El numero de telefono que ingreso esta en uso.
+                      </div>
+                    <?php
+                  }
+
+              }
+
+              $conn->close();
+
+              //Inicia seguridad en la base de datos
+              function get_post($conn, $var) {
+                return $conn->real_escape_string($_POST[$var]);
+              }
+
+
+              ?>
+          </form>
+        </div>
+
+
+      </div>
+    </section>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/FileSaver.js"></script>
+
+  </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>CarWash</title>
+  </head>
+
+  <body>
+
+    <div class="content">
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="col-md-6 contents">
+            <div class="row justify-content-center">
+              <div class="col-md-8">
+                <div class="mb-4">
+                <h3>Iniciar sesión</h3>
+                <p class="mb-4">Ingrese sus datos para continuar.</p>
+              </div>
+              <form action="index.php" method="post">
+                <div class="form-group first">
+                  <label for="username">Correo</label>
+                  <input type="text" class="form-control" id="username" name="user">
+
+                </div>
+                <div class="form-group last mb-4">
+                  <label for="password">Contraseña</label>
+                  <input type="password" class="form-control" id="password" name="pass">
+                </div>
+
+                <div class="d-flex mb-5 align-items-center">
+                  <label class="control control--checkbox mb-0"><span class="caption">Recuerdame</span>
+                    <input type="checkbox" checked="checked"/>
+                    <div class="control__indicator"></div>
+                  </label>
+                  <span class="ml-auto"><a href="CambioContra.php" class="forgot-pass">Olvide mi contraseña</a></span>
+                </div>
+
+                <button type="submit" name="enviar" value="submit" class="btn btn-block btn-primary">CONTINUAR</button>
+
+                <?php
+                //Se entra al darle click al boton de Continuar
+                if (isset($_POST['enviar'])) {
+                  require_once 'login.php';
+                  $conn = new mysqli($hn, $un, $pw, $db);
+                  if ($conn->connect_error) die("Error en Conexion");
+
+                  //Obtiene el usuario y la contraseña de los campos ingresados
+                  $usuario =  $_POST['user'];
+                  $contraseña =  $_POST['pass'];
+
+                  //Obtiene toda la informacion en base al usuario y la contraseña
+                  $query = "SELECT ID, nombre, num_Telefono, fechanacimiento, correo, contrasenia FROM usuarios WHERE correo = '$usuario' && contrasenia = '$contraseña' ";
+                  $result = $conn->query($query);
+
+                  if (!$result) die("Fatal Error");
+
+                  $row = $result->fetch_array(MYSQLI_ASSOC);
+
+                  //Validacion que el usuario y la contraseña no esten vacios y que los valores ingresados sean iguales a los guardados en la base de datos
+                  if (($usuario != "" && $contraseña != "") && (($usuario == $row['correo']) && ($contraseña == $row['contrasenia']))) {
+
+                    //Guardamos las variables de los campos en variables de sesion
+                    session_start();
+                    $_SESSION['id'] = $row['ID'];
+                    $_SESSION['Nom'] = $row['nombre'];
+                    $_SESSION['N_Tel'] = $row['num_telefono'];
+                    $_SESSION['N_Fecha'] = $row['fechanacimiento'];
+                    $_SESSION['Correo'] = $row['correo'];
+                    $_SESSION['Contra'] = $row['contrasenia'];
+
+                    //Reedirige al usuario a la creacion de perfil al darle enviar
+                    if (isset($_POST['enviar'])){
+                      ?>
+                        <script type="text/javascript">
+                          window.location = "main.php";
+                        </script>
+                      <?php
+                    }
+
+                  //Si el usuario o la contraseña estan vacios les genera esta alerta
+                  } else if($usuario == "" || $contraseña == "") {
+                    ?>
+                      <div class="alert">
+                        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                        <strong>Error!</strong> Correo o contraseña no ingresada.
+                      </div>
+                    <?php
+                  //Si el usuario o la contraseña son diferentes a los de la base de datos les genera esta alerta
+                  } else if(($usuario != $row['correo']) && ($contraseña != $row['contrasenia'])) {
+                    ?>
+                      <div class="alert">
+                        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                        <strong>Error!</strong> Correo o contraseña incorrecta.
+                      </div>
+                    <?php
+                  }
+                  $result->close();
+                  $conn->close();
+                }
+                ?>
+
+                <div class="d-flex mb-5 align-items-center">
+                  <span class="ml-left"><a href="Registro.php" class="forgot-pass">Crear una cuenta</a></span>
+                  <span class="ml-auto" ><a href="Registro.php" class="forgot-pass">Ingresar como invitado</a></span>
+                </div>
+
+                </form>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+
+</html>
